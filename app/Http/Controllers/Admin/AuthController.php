1a3b9fada37e7cdf462163188b627d28a3afcac6
Rhuan Yago <rhuan@made4it.com.br>
2022-02-05 15:38:58 -0300

    first commit

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace LaraDev\Http\Controllers\Admin;
+
+use LaraDev\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use LaraDev\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        if (Auth::check() === true) {
+            return redirect()->route('admin.home');
+        }
+
+        return view('admin.index');
+    }
+
+    public function home()
+    {
+        return view('admin.dashboard');
+    }
+
+    public function login(Request $request)
+    {
+        if (in_array('', $request->only('email', 'password'))) {
+            $json['message'] = $this->message->error('Oooops, informe todos os dados para efetuar o login!')->render();
+
+            return response()->json($json);
+        }
+
+        if (!filter_var($request->email, FILTER_VALIDATE_EMAIL)) {
+            $json['message'] = $this->message->error('Oooops, informe um e-mail válido!')->render();
+
+            return response()->json($json);
+        }
+
+        $credentials = [
+            'email'     => $request->email,
+            'password'  => $request->password
+        ];
+
+        if (!Auth::attempt($credentials)) {
+            $json['message'] = $this->message->error('Oooops, usuário e/ou senha não conferem!')->render();
+
+            return response()->json($json);
+        }
+
+        $this->authenticated($request->getClientIp());
+        
+        $json['redirect'] = route('admin.home');
+        return response()->json($json);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('admin.login');
+    }
+
+    private function authenticated(string $ip)
+    {
+        $user = User::where('id', Auth::user()->id);
+        $user->update([
+            'last_login_at' => date('Y-m-d H:i:s'),
+            'last_login_ip' => $ip
+        ]);
+    }
+}
